84e4f69c1e5e32a0a2ff75f547fbb38ab49d6f7a
Joseph Harry <findarato@gmail.com>
2011-12-21 20:12:51 +0000

    Adding the boilerplat in

--- parseServerStatus.php
+++ parseServerStatus.php
@@ -1,22 +1,25 @@
 <?php
-	include_once("db.php");  
-
-	preg_match("/([0-9]*)/",$isbn,$matches);
-	$isbn = $matches[0];
+	include_once("db.class.php");  
+	$db = db::getInstance();
 	$url = "http://www.swtor.com/server-status";
-
 	$doc = new DOMDocument();
 	@$doc->loadHTMLFile($url);
-	
 	$record = $doc->getElementsByTagName('div');
-
 	if (!is_null($record)) { //you are not parsing the marc record view
 		foreach ($record as $r) {
-			echo $r->attributes->getNamedItem("data-status")->nodeValue;
-			echo $r->attributes->getNamedItem("data-name")->nodeValue;
-			echo $r->attributes->getNamedItem("data-population")->nodeValue;
-			echo $r->attributes->getNamedItem("data-type")->nodeValue;
-			echo $r->attributes->getNamedItem("data-timezone")->nodeValue;
+			if($r->attributes->getNamedItem("data-status")){
+				$status = $r->attributes->getNamedItem("data-status")->nodeValue;
+				$name = $db->Clean($r->attributes->getNamedItem("data-name")->nodeValue);
+				$pop = $r->attributes->getNamedItem("data-population")->nodeValue;
+				$type = $r->attributes->getNamedItem("data-type")->nodeValue;
+				if($r->attributes->getNamedItem("data-timezone")){
+					$location = $r->attributes->getNamedItem("data-timezone")->nodeValue;	
+				}else{
+					$location = $r->attributes->getNamedItem("data-language")->nodeValue;
+				}
+				$db->Query("INSERT INTO serverStatus VALUES(NOW(),'".$status."','".$name."','".$pop."','".$type."','".$location."');");
+				print_r($db->Error);
+			}
 		}
 	}
 
